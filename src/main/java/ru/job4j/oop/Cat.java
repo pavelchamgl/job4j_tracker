c212055678edbe2124f3722003f87d25b5f4d5ea
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2020-07-12 20:31:25 +0300

    1.5. Поля объекта [#311575] Исправление недочетов в задании

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -11,8 +11,7 @@ public class Cat {
     }
 
     public void show() {
-        System.out.println(this.name);
-        System.out.println(this.food);
+        System.out.println("There are " + this.name + "'s food - " + this.food);
     }
 
     public void eat(String meat) {
@@ -20,12 +19,12 @@ public class Cat {
     }
 
     public static void main(String[] args) {
-        System.out.println("There are gav's food.");
         Cat gav = new Cat();
+        gav.giveNick("gav");
         gav.eat("kotleta");
         gav.show();
-        System.out.println("There are black's food.");
         Cat black = new Cat();
+        black.giveNick("black");
         black.eat("fish");
         black.show();
     }
