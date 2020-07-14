0c34ef5fa7530c3fc9b5e7e262e821ac1a4cc98c
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2020-07-14 23:05:30 +0300

    5. Tracker - хранилище [#311614]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,18 +2,29 @@ package ru.job4j.tracker;
 
 public class Item {
 
-    private String  id;
+    private int  id;
     private String name;
 
     public Item() {
     }
 
-    public Item(String id) {
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
         this.id = id;
     }
 
-    public Item(String id, String name) {
-        this.id = id;
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 }
