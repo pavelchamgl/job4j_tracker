f4559cc45ff379662e9d7a3c8fc2eb22c574fb9b
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2020-07-23 09:20:58 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#311560]

---
+++ src/test/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        Fact fc = new Fact();
+        fc.calc(-1);
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Значение должно быть больше нуля.");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
